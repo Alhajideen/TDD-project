6e41fc24da26d932a9ccac044458983fe66b0d4e
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-06 02:09:10 +0300

    Add test for any other case

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -40,5 +40,8 @@ describe 'reverse' do
     it 'Expect to return "buzz" when N is divisible by 3 and 5' do
       expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
     end
+    it 'Expect to return N is a string' do
+      expect(@solver.fizzbuzz(7)).to eql "7"
   end
+end
 end
